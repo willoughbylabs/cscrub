507edcf98b9e4a227df519838006bb131286b574
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2022-04-05 09:57:22 -0500

    Add start and quit webdriver functions

--- webdriver.py
+++ webdriver.py
@@ -3,13 +3,24 @@ from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import time
 
-driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 
-driver.get("https://bing.com")
-time.sleep(5)
-element = driver.find_element_by_id("sb_form_q")
-element.send_keys("WebDriver")
-element.submit()
+def start_webdriver():
+    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    return driver
 
-time.sleep(5)
-driver.quit()
+
+def quit_webdriver(driver):
+    driver.quit()
+
+
+def search_bing():
+    driver = start_webdriver()
+
+    driver.get("https://bing.com")
+    time.sleep(5)
+    element = driver.find_element_by_id("sb_form_q")
+    element.send_keys("WebDriver")
+    element.submit()
+    time.sleep(5)
+
+    quit_webdriver(driver)
