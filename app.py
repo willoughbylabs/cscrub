b29dc73353d89206722a982e49e9642fe7721489
Andrew Willoughby <30395608+willoughbylabs@users.noreply.github.com>
2022-04-05 17:00:33 -0500

    Fetch list of past and present city council members

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from models.helpers import db_connect
 from models.members import Alderperson
 
 # CONFIGURATIONS
-create_tables_in_db = True
+create_tables_in_db = False
+fetch_members = True
 
+# APP
 if create_tables_in_db:
     db_connect.create_tables()
+
+if fetch_members:
+    members = Alderperson.fetch_members()
+    print(members)
